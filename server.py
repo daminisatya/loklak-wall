13970d9a36dfb98669def3546dd469825f5fb099
sudheesh001 <sudheesh1995@outlook.com>
2016-04-30 19:30:21 +0530

    Dynamic updates, image cleanups

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os,binascii
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, Blueprint
+     render_template, flash, Blueprint, Response
 from flask import send_from_directory
 from flask.ext.responses import json_response, xml_response, auto_response
 import datetime, chartkick
@@ -10,6 +10,9 @@ import logging
 from loklak import Loklak
 from collections import Counter
 import requests
+import time
+
+from config import *
 
 app = Flask(__name__)
 ck = Blueprint('ck_page', __name__, static_folder=chartkick.js(), static_url_path='/static')
@@ -21,9 +24,11 @@ def yieldFun():
     def inner():
         l = Loklak()
         tweets = l.search('fossasia')
-        for tweet in tweets.statuses:
-            time.sleep(1)                           # Don't need this just shows the text streaming
-            yield tweet
+        # for tweet in tweets['statuses']:
+        #     time.sleep(1)                           # Don't need this just shows the text streaming
+        #     yield tweet.text
+        for i in range(0,10000):
+        	yield i
     return flask.Response(inner(), mimetype='text/html')  # text/html is required for most browsers to show th$
 
 @app.route('/ajax/wall/<query>', methods=['GET'])
@@ -31,6 +36,14 @@ def ajaxwall(query=None):
 	q = query
 	l = Loklak()
 	tweets = l.search(q)
+	for tweet in tweets['statuses']:
+		try:
+			imageUrl = tweet['user']['profile_image_url_https']
+			newUrl = imageUrl.split('_bigger')
+			updatedUrl = newUrl[0]+newUrl[1]
+			tweet['user']['profile_image_url_https'] = updatedUrl
+		except:
+			pass
 	return json_response(tweets)
 
 @app.route('/wall/<query>', methods=['GET'])
@@ -38,7 +51,15 @@ def wall(query=None):
 	q = query
 	l = Loklak()
 	tweets = l.search(q)
-	return render_template('index.html', name=q, tweets=tweets)
+	for tweet in tweets['statuses']:
+		try:
+			imageUrl = tweet['user']['profile_image_url_https']
+			newUrl = imageUrl.split('_bigger')
+			updatedUrl = newUrl[0]+newUrl[1]
+			tweet['user']['profile_image_url_https'] = updatedUrl
+		except:
+			pass
+	return render_template('index.html', name=q, tweets=tweets, BASE_URL=BASE_URL)
 
 
 @app.route('/', methods=['GET', 'POST'])
